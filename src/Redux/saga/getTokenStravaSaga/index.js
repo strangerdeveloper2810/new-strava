30bfeb3e28e8d942863547b8a616045cc04c2600
Trinh Nguyen <nguyenmaster3012@gmail.com>
2024-07-06 04:37:33 +0700

    save data in localStorage

--- src/Redux/saga/getTokenStravaSaga/index.js
+++ src/Redux/saga/getTokenStravaSaga/index.js
@@ -1,6 +1,6 @@
 import { put, call, takeLatest } from "redux-saga/effects";
 import { FETCH_API_GET_TOKEN_STRAVA } from '../../types/index';
-import { clientId, clientSecret, httpFetchToken, redirectUri, scope } from "../../../Settings";
+import { clientId, clientSecret, httpFetchToken, redirectUri, scope, settings } from "../../../Settings";
 
 export function* getTokenStrava() {
     try {
@@ -22,6 +22,16 @@ export function* getTokenStrava() {
             const response = yield call(() => {
                 return httpFetchToken.post("/oauth/token", null, { params })
             });
+
+            const accessToken = response.data.access_token;
+            const refreshToken = response.data.refresh_token;
+            const athleteId = response.data.athlete.id;
+            const fullName = `${response.data.athlete.firstname} ${response.data.athlete.lastname}`;
+
+            settings.setStorage(accessToken);
+            settings.setStorage(refreshToken);
+            settings.setStorage(athleteId);
+            settings.setStorage(fullName);
 
             console.log({ response });
         }
